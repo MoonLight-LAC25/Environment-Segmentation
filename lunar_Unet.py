3cf4e746451ef1250ba40b20b88e45db4df4003c
lunar-autonomy <lunar-autonomy@airstation-01.lan.local.cmu.edu>
2025-04-27 03:45:31 -0400

    Merge

--- lunar_Unet.py
+++ lunar_Unet.py
@@ -6,6 +6,7 @@ import cv2
 
 import sys
 import os
+import cv2 
 
 # # Get the absolute path of the parent directory
 current_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))
@@ -18,7 +19,12 @@ from image_utils import ImageUtils
 
 class Lunar_UNet(Unet):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        lunar_kwargs = {'count':False, 'input_shape':[640, 640], 'num_classes':4, 'classes':["background","surface","rocks","lander"], 'model_path':"team_code/Perceptron/MAC_SLAM_Lunar/Model/UNet/best_epoch_weights.pth"}
+        combined_args = {**lunar_kwargs, **kwargs}
+        super().__init__(**combined_args)
+        self.image_utils = ImageUtils()
+        self.prev_mask = None
+        self.mask_store = None
 
     def get_class_mask(self, mask, class_pixels_rgb):
         mask = np.array(mask)
@@ -30,7 +36,37 @@ class Lunar_UNet(Unet):
                 for width in range(mask.shape[1]):
                     if (mask[height][width] == class_pixels_rgb).all(): 
                       mod_img[height][width] = mask[height][width][0]
-        return Image.fromarray(np.uint8(mod_img))
+        #return Image.fromarray(np.uint8(mod_img))
+    @staticmethod
+    def erode_class(segmentation_map, target_class, bg_class, kernel_size=3, iterations=1):
+        mask = (segmentation_map == target_class).astype(np.uint8)
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_size, kernel_size))
+        eroded_mask = cv2.erode(mask, kernel, iterations=iterations)
+        new_segmentation_map = segmentation_map.copy()
+        new_segmentation_map[(mask == 1) & (eroded_mask == 0)] = bg_class
+        return new_segmentation_map
+    
+    def set_mask(self, image):
+        try:
+            self.this_mask = self.erode_class(np.uint8(self.detect_image(image)), target_class=2, bg_class=1)
+            self.mask_store = self.this_mask
+        except Exception as e:
+            print(f"Error in set_mask: {e}")
+            raise
+    
+    def get_binary_rock_mask(self):
+        if self.this_mask is None:
+            raise ValueError("Mask not set. Call set_mask() first to get rock mask.")
+        return self.this_mask == 2
+    
+    def get_binary_lander_mask(self):
+        if self.this_mask is None:
+            raise ValueError("Mask not set. Call set_mask() first to get lander mask.")
+        return self.this_mask == 3
+    
+    def clear_mask(self):
+        self.this_mask = None
 
 if __name__ == "__main__":
     # Test initialisation for model
